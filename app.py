6df1c0ca41a502ff67932b018d9b35c9cb107371
davybayet <82160438+davybayet@users.noreply.github.com>
2023-02-21 22:44:29 +0100

    Add files via upload

---
+++ app.py
@@ -0,0 +1,57 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[8]:
+
+
+import streamlit as st
+import pandas as pd
+import plotly.express as px
+
+# Load data
+df = pd.read_csv('s23-recrutement-until-now.csv', encoding="ISO-8859-1")
+
+# Set up the app
+st.set_page_config(page_title='S23 Recruitment Dashboard')
+
+# Add a title to the page
+st.title('Analytics Dashboard of Recruitment Cohorte S23')
+
+# Allow the user to choose a program
+program_chosen = st.selectbox("Choose program of interest:", df["Program"].unique())
+
+# Filter the data based on the program chosen
+df_program = df[df["Program"]==program_chosen]
+
+# Add a histogram of math results
+st.header("Math Results Histogram")
+fig_hist = px.histogram(df_program, x="Math Result")
+st.plotly_chart(fig_hist)
+
+# Add a strip plot of math results by nationality
+st.header("Math Results Strip Plot by Nationality")
+fig_strip = px.strip(df_program, x="Math Result", y="Nationality")
+st.plotly_chart(fig_strip)
+
+# Add a sunburst plot of programs, nationalities, and cities
+df_sburst = df_program[df_program["Mode"].isin(["Alternant","Traditional","SPOC"])]
+st.header("Programs, Nationalities, and Cities Sunburst Plot")
+fig_sunburst = px.sunburst(df_sburst, path=["Program", "Nationality", "City"])
+st.plotly_chart(fig_sunburst)
+
+# Add an ECDF plot of math results
+st.header("Math Results ECDF")
+fig_ecdf = px.ecdf(df_program, x="Math Result")
+st.plotly_chart(fig_ecdf)
+
+# Add a line plot of math results by date and gender
+st.header("Math Results by Mode and Gender Line Plot")
+fig_line = px.line(df_program, x="Mode", y="Math Result", color="Gender", markers=True)
+st.plotly_chart(fig_line)
+
+
+# In[ ]:
+
+
+
+
