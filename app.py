127bc1fdbc0a6cbad32bfb01e8dfadec39a5c12e
davybayet <82160438+davybayet@users.noreply.github.com>
2023-02-22 14:11:56 +0100

    Update app.py

--- app.py
+++ app.py
@@ -24,13 +24,13 @@ program_chosen = st.selectbox("Choose program of interest:", df["Program"].uniqu
 df_program = df[df["Program"]==program_chosen]
 
 # Add a histogram of math results
-st.header("Math Results Histogram")
-fig_hist = px.histogram(df_program, x="Math Result")
+st.header("Birth Date Histogram")
+fig_hist = px.histogram(df_program, x="Birth Date-Yearly")
 st.plotly_chart(fig_hist)
 
 # Add a strip plot of math results by nationality
-st.header("Math Results Strip Plot by Nationality")
-fig_strip = px.strip(df_program, x="Math Result", y="Nationality")
+st.header("Birth Date Strip Plot by Nationality")
+fig_strip = px.strip(df_program, x="Birth Date-Yearly", y="Nationality")
 st.plotly_chart(fig_strip)
 
 # Add a pie plot of nationality by mode
@@ -40,18 +40,18 @@ st.plotly_chart(fig_pie)
 
 # Add a sunburst plot of programs, nationalities, and cities
 df_sburst = df_program[df_program["Mode"].isin(["Alternant","Traditional","SPOC"])]
-st.header("Programs, Nationalities, and Cities Sunburst Plot")
-fig_sunburst = px.sunburst(df_sburst, path=["Program", "Nationality", "City"])
+st.header("Programs, Nationalities Sunburst Plot")
+fig_sunburst = px.sunburst(df_sburst, path=["Program", "Nationality"])
 st.plotly_chart(fig_sunburst)
 
 # Add an ECDF plot of math results
-st.header("Math Results ECDF")
-fig_ecdf = px.ecdf(df_program, x="Math Result")
+st.header("Birth Date-Yearly ECDF")
+fig_ecdf = px.ecdf(df_program, x="Birth Date-Yearly")
 st.plotly_chart(fig_ecdf)
 
 # Add a line plot of math results by date and gender
-st.header("Math Results by Mode Line Plot")
-fig_line = px.line(df_program, x="Mode", y="Math Result", markers=True)
+st.header("Birth Date-Yearly by Mode Line Plot")
+fig_line = px.line(df_program, x="Mode", y="Birth Date-Yearly", markers=True)
 st.plotly_chart(fig_line)
 
 
