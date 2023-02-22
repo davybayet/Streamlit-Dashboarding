9a7121d6a9fdf8704e5bb855fd0b797fc8b057ca
davybayet <82160438+davybayet@users.noreply.github.com>
2023-02-22 14:23:23 +0100

    Update app.py

--- app.py
+++ app.py
@@ -25,12 +25,12 @@ df_program = df[df["Program"]==program_chosen]
 
 # Add a histogram of math results
 st.header("Birth Date Histogram")
-fig_hist = px.histogram(df_program, x="Birth Date - Yearly")
+fig_hist = px.histogram(df_program, x="Birth Date")
 st.plotly_chart(fig_hist)
 
 # Add a strip plot of math results by nationality
 st.header("Birth Date Strip Plot by Nationality")
-fig_strip = px.strip(df_program, x="Birth Date - Yearly", y="Nationality")
+fig_strip = px.strip(df_program, x="Birth Date", y="Nationality")
 st.plotly_chart(fig_strip)
 
 # Add a pie plot of nationality by mode
@@ -46,12 +46,12 @@ st.plotly_chart(fig_sunburst)
 
 # Add an ECDF plot of math results
 st.header("Birth Date-Yearly ECDF")
-fig_ecdf = px.ecdf(df_program, x="Birth Date - Yearly")
+fig_ecdf = px.ecdf(df_program, x="Birth Date")
 st.plotly_chart(fig_ecdf)
 
 # Add a line plot of math results by date and gender
 st.header("Birth Date-Yearly by Mode Line Plot")
-fig_line = px.line(df_program, x="Mode", y="Birth Date - Yearly", markers=True)
+fig_line = px.line(df_program, x="Mode", y="Birth Date", markers=True)
 st.plotly_chart(fig_line)
 
 
